2d13cebd867c76156fcbb6df0f18b105f0c23caf
kasper <Kirillpc1@gmail.com>
2026-02-26 17:52:51 +0300

    12345

--- src/app/bot_config/config.py
+++ src/app/bot_config/config.py
@@ -1,28 +1,46 @@
 import os
+from enum import StrEnum
 from typing import Final
 
+from aiogram.enums import ParseMode
 from dotenv import load_dotenv
 from aiogram import Bot
 from aiogram.client.default import DefaultBotProperties
 
+from app.logger import get_logger, LogSetupPreset
 from app.utils import shifted_id
 
+logger = get_logger()
 
-load_dotenv(dotenv_path='.env')
+if not load_dotenv(dotenv_path='.env'):
+    logger.critical("Can't load .env file")
+    raise RuntimeError(".env file not loaded")
 
+
+class EnvironmentType(StrEnum):
+    DEV = 'dev'
+    PROD = 'prod'
 
 # --------- Getter .env
 BOT_TOKEN = str(os.getenv("BOT_TOKEN"))
 CHAT_ID: Final[str] = os.getenv("CHAT_ID", "")
 LOG_CHAT_ID: Final[str] = os.getenv("LOG_CHAT_ID", "0")
-print(LOG_CHAT_ID)
-print(CHAT_ID)
+LOG_LEVEL: Final[str] = os.getenv("LOG_LEVEL", "DEBUG").upper()
+ENVIRONMENT: Final[EnvironmentType] = EnvironmentType(
+    os.getenv("ENVIRONMENT", EnvironmentType.DEV)
+)
+
+LOGGER_PRESET = (
+    LogSetupPreset.DEV_CONSOLE
+    if ENVIRONMENT.DEV
+    else LogSetupPreset.PRODUCTION
+)
 
 
 # --------- Bot config
 bot = Bot(
     token=BOT_TOKEN,
-    default=DefaultBotProperties(parse_mode='html')
+    default=DefaultBotProperties(parse_mode=ParseMode.HTML)
 )
 
 # xyi
--- src/app/core/setup.py
+++ src/app/core/setup.py
@@ -1,17 +1,17 @@
 import asyncio
-import logging
 
-
-from app.bot_config.config import bot, CHAT_ID, SHIFTED_CHAT_ID, LOG_CHAT_ID, SHIFTED_LOG_CHAT_ID
+from app.bot_config.config import bot, CHAT_ID, SHIFTED_CHAT_ID, LOG_CHAT_ID, SHIFTED_LOG_CHAT_ID, LOG_LEVEL, LOGGER_PRESET
 from app.callback import start_command_callback
 from app.handler import events
 from app.handler.start_ import dp
-from app.logger import IwfLogger
+from app.logger import IwfLogger, get_logger
 
-logging.basicConfig(level=logging.INFO)
+logger = get_logger()
 
 
 async def main() -> None:
+    logger.info("Starting up application")
+
     dp["chat_id"] = CHAT_ID
     dp["shifted_chat_id"] = SHIFTED_CHAT_ID
     dp["log_chat_id"] = LOG_CHAT_ID
@@ -21,8 +21,10 @@ async def main() -> None:
         events.rt
     )
 
+    logger.info("Aiogram workspace configured. Starting polling...")
     await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    IwfLogger.setup("DEBUG")
+    IwfLogger.setup(LOG_LEVEL, LOGGER_PRESET)
+    logger.debug("Logger configured")
     asyncio.run(main())
